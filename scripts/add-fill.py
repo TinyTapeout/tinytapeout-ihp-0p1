a633047b51dfc6f9905ec16aee2b9dc2434bf872
htfab <mpw@htamas.net>
2024-07-24 03:56:03 +0200

    feat: increase spacing for active layer only

--- scripts/add-fill.py
+++ scripts/add-fill.py
@@ -11,16 +11,15 @@ output_gds = f"{results_dir}/6_final_fill.gds"
 die_size = (2200, 2200)
 die_margin = 36
 fill_grid = 6
-fill_spacing = 4
 fill_groups = [
-    [("Activ", (1, 0), 4),
-    ("GatPoly", (5, 0), 4),
-    ("NWell", (31, 0), 5)],
-    [("Metal1", (8, 0), 5)],
-    [("Metal2", (10, 0), 5)],
-    [("Metal3", (30, 0), 5)],
-    [("Metal4", (50, 0), 5)],
-    [("Metal5", (67, 0), 5)],
+    ([("Activ", (1, 0), 4),
+      ("GatPoly", (5, 0), 4),
+      ("NWell", (31, 0), 5)], 8),
+    ([("Metal1", (8, 0), 5)], 4),
+    ([("Metal2", (10, 0), 5)], 4),
+    ([("Metal3", (30, 0), 5)], 4),
+    ([("Metal4", (50, 0), 5)], 4),
+    ([("Metal5", (67, 0), 5)], 4),
 ]
 fill_exclude = [
     ('dfpad', (41, 0)),
@@ -32,8 +31,6 @@ top = layout.top_cell()
 u = round(1/layout.dbu)
 
 fill_box = pya.Box(die_margin*u, die_margin*u, (die_size[0]-die_margin)*u, (die_size[1]-die_margin)*u)
-cell_box = pya.Box(-fill_grid*u//2-fill_spacing*u, -fill_grid*u//2-fill_spacing*u,
-                   (fill_grid*u+1)//2+fill_spacing*u, (fill_grid*u+1)//2+fill_spacing*u)
 row_step = pya.Vector(fill_grid*u, 0)
 column_step = pya.Vector(0, fill_grid*u)
 fill_margin = pya.Vector(0, 0)
@@ -45,11 +42,13 @@ for layer_name, (layer, datatype) in fill_exclude:
     exclude_region = pya.Region(top.begin_shapes_rec(layer_index))
     fill_region_init -= exclude_region
 
-for fill_layers in fill_groups:
+for fill_layers, fill_spacing in fill_groups:
     layer_list = [layer_name for layer_name, *_ in fill_layers]
     print(f"Generating fill for {' & '.join(layer_list)}...")
     cell_index = layout.add_cell(f"FILL_{layer_list[0]}")
     cell_obj = layout.cell(cell_index)
+    cell_box = pya.Box(-fill_grid*u//2-fill_spacing*u, -fill_grid*u//2-fill_spacing*u,
+                       (fill_grid*u+1)//2+fill_spacing*u, (fill_grid*u+1)//2+fill_spacing*u)
     fill_region = fill_region_init.dup()
     for layer_name, (layer, datatype), size in fill_layers:
         layer_index = layout.layer(layer, datatype)
