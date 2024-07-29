09f4fe2be8b1939574c60e462d4bae4a7c75ab45
htfab <mpw@htamas.net>
2024-07-29 15:08:31 +0200

    fix(tt_um_kianV_rv32ima_uLinux_SoC): apply patch from Hirosh
    don't toggle the DQ input to the flash at the rising edge of the clock

--- projects/tt_um_kianV_rv32ima_uLinux_SoC/src/qqspi.v
+++ projects/tt_um_kianV_rv32ima_uLinux_SoC/src/qqspi.v
@@ -108,7 +108,7 @@ module p23_qqspi #(
   always @(posedge clk) begin
     if (!resetn) begin
       ce <= ~0;
-      sclk <= 1'b0;
+      sclk <= 1'b1;
       sio_oe <= 4'b0000;
       sio_out <= 4'b0000;
       spi_buf <= 0;
