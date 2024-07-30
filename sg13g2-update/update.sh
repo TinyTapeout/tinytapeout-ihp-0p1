cac600fdbb993d966fdcaef593b7f7f330e5a0d5
htfab <mpw@htamas.net>
2024-07-30 11:10:14 +0200

    feat: increase Metal2 minimum area in tech lef

--- sg13g2-update/update.sh
+++ sg13g2-update/update.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 export UPDATE_DIR=$(dirname $(realpath $0))
-cd ${ORFS_ROOT:-orfs}/flow/platforms/ihp-sg13g2
+export ORFS_ROOT=${ORFS_ROOT:-orfs}
+cd $ORFS_ROOT/flow/platforms/ihp-sg13g2
 python $UPDATE_DIR/sg13g2-update.py
 cp $UPDATE_DIR/config.mk .
+
+# patch Metal2 minimum area rule
+sed -i -e '/^LAYER Metal2$/,/^END Metal2$/!b' -e '/^ *AREA /!b' -e 's/0\.144/0.18/' $ORFS_ROOT/flow/platforms/ihp-sg13g2/lef/sg13g2_tech.lef
