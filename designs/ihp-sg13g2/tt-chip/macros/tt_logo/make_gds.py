6ee79f6c04c5d8f9d77b39fbc5f0d30010cc4d7a
htfab <mpw@htamas.net>
2024-07-30 18:23:05 +0200

    feat: redraw the logo so that it doesn't need slits

--- designs/ihp-sg13g2/tt-chip/macros/tt_logo/make_gds.py
+++ designs/ihp-sg13g2/tt-chip/macros/tt_logo/make_gds.py
@@ -16,10 +16,10 @@ PIXEL_SIZE = 2.0 # um
 # Convert the image to grayscale
 img = img.convert("L")
 
-slits = set()
-for j in range(img.height//6):
-    for i in range(img.width//8):
-        slits.add((i, j))
+#slits = set()
+#for j in range(img.height//6):
+#    for i in range(img.width//8):
+#        slits.add((i, j))
 
 layout = gdspy.Cell("tt_logo")
 for y in range(img.height):
@@ -32,22 +32,22 @@ for y in range(img.height):
                 gdspy.Rectangle((x * PIXEL_SIZE, flipped_y * PIXEL_SIZE),
                                 ((x + 1) * PIXEL_SIZE, (flipped_y + 1) * PIXEL_SIZE),
                                 layer=LAYER, datatype=DATATYPE_DRAWING))
-        else:
-            if 1 <= flipped_y % 6 <= 4:
-                slits.discard((x//8, flipped_y//6))
+        #else:
+        #    if 1 <= flipped_y % 6 <= 4:
+        #        slits.discard((x//8, flipped_y//6))
 
-for i, j in set(slits):
-    if (i + j) % 2 == 0:
-        slits.discard((i-1, j))
-        slits.discard((i+1, j))
-        slits.discard((i, j-1))
-        slits.discard((i, j+1))
+#for i, j in set(slits):
+#    if (i + j) % 2 == 0:
+#        slits.discard((i-1, j))
+#        slits.discard((i+1, j))
+#        slits.discard((i, j-1))
+#        slits.discard((i, j+1))
 
-for i, j in slits:
-    layout.add(
-        gdspy.Rectangle(((8 * i + 1) * PIXEL_SIZE, (6 * j + 2) * PIXEL_SIZE),
-                        ((8 * i + 7) * PIXEL_SIZE, (6 * j + 4) * PIXEL_SIZE),
-                        layer=LAYER, datatype=DATATYPE_SLIT))
+#for i, j in slits:
+#    layout.add(
+#        gdspy.Rectangle(((8 * i + 1) * PIXEL_SIZE, (6 * j + 2) * PIXEL_SIZE),
+#                        ((8 * i + 7) * PIXEL_SIZE, (6 * j + 4) * PIXEL_SIZE),
+#                        layer=LAYER, datatype=DATATYPE_SLIT))
 
 # Save the layout to a file
 gdspy.write_gds("gds/tt_logo.gds")
